6256c8f9783a6c5aa5962de1b0a24d99d5072bc7
tommy stevan <magui.maggie@gmail.com>
2023-08-16 15:54:52 -0300

    authentication and authorization

---
+++ src/main/java/com/mindhub/homebanking/configurations/WebAuthorization.java
@@ -0,0 +1,63 @@
+package com.mindhub.homebanking.configurations;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.WebAttributes;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@EnableWebSecurity
+@Configuration
+public class WebAuthorization {
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+
+                .antMatchers("/web/index.html","/web/css/**","/web/img/**","/web/js/index.js").permitAll()
+                .antMatchers("/rest/**","/h2-console").hasAuthority("ADMIN")
+
+
+                .antMatchers("/web/**").hasAuthority("CLIENT");
+                //.antMatchers("/web/**").hasAuthority("CLIENT").;
+
+        http.formLogin()
+
+                .usernameParameter("email")
+
+                .passwordParameter("password")
+
+                .loginPage("/api/login");
+        http.logout().logoutUrl("/api/logout");
+
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+        http.exceptionHandling().authenticationEntryPoint((req, res, exc) -> res.sendError(HttpServletResponse.SC_UNAUTHORIZED));
+        http.formLogin().successHandler((req, res, auth) -> clearAuthenticationAttributes(req));
+        http.formLogin().failureHandler((req, res, exc) -> res.sendError(HttpServletResponse.SC_UNAUTHORIZED));
+        http.logout().logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler());
+
+        return http.build();
+    }
+
+
+
+    private void clearAuthenticationAttributes(HttpServletRequest request) {
+
+        HttpSession session = request.getSession(false);
+
+        if (session != null) {
+
+            session.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
+
+        }
+    }
+}
--- src/main/java/com/mindhub/homebanking/repositories/ClientRepository.java
+++ src/main/java/com/mindhub/homebanking/repositories/ClientRepository.java
@@ -8,5 +8,5 @@ import java.util.List;
 
 @RepositoryRestResource
 public interface ClientRepository extends JpaRepository<Client,Long> {
-    List<Client> findByEmail(String email);
+    Client findByEmail(String email);
 }
