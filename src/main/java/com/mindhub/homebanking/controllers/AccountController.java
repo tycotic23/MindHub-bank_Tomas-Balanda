9bd1a1b19e711aed1b773d6cabe24f68a92fbab8
tommy stevan <magui.maggie@gmail.com>
2023-08-17 18:36:12 -0300

    add account

--- src/main/java/com/mindhub/homebanking/controllers/AccountController.java
+++ src/main/java/com/mindhub/homebanking/controllers/AccountController.java
@@ -1,12 +1,17 @@
 package com.mindhub.homebanking.controllers;
 
 import com.mindhub.homebanking.dtos.AccountDTO;
+import com.mindhub.homebanking.models.Account;
+import com.mindhub.homebanking.models.Client;
 import com.mindhub.homebanking.repositories.AccountRepository;
+import com.mindhub.homebanking.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -15,6 +20,8 @@ import java.util.stream.Collectors;
 public class AccountController {
     @Autowired
     private AccountRepository accountRepository;
+    @Autowired
+    private ClientRepository clientRepository;
 
     @RequestMapping("/accounts")
     public List<AccountDTO> getAll(){
@@ -25,4 +32,30 @@ public class AccountController {
     public AccountDTO getAccount(@PathVariable Long id){
         return accountRepository.findById(id).map(AccountDTO::new).orElse(null);
     }
+
+    @RequestMapping(path ="/clients/current/accounts" ,method = RequestMethod.POST)
+    public ResponseEntity<Object> createAccount(Authentication authentication){
+        //verificar que cumpla con todas las condiciones
+        //si cumple las condiciones se crea una cuenta
+        if(accountRepository.findByClient_email(authentication.getName()).size()>=3){
+            return new ResponseEntity<>("User has 3 accounts", HttpStatus.FORBIDDEN);
+        }
+        //crear la nueva cuenta
+        Account account = new Account(createNumberAccount(),LocalDate.now(),0.0);
+        account.setClient(clientRepository.findByEmail(authentication.getName()));
+        accountRepository.save(account);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    private String createNumberAccount(){
+        //crea un numero aleatorio entre el 0 y el 99999999 de la forma: VIN-nnnnnnnn
+        int random= (int) (Math.random() * 99999999);
+        return "VIN-"+random;
+    }
+
+    //metodo de prueba
+    /*@RequestMapping("/accounts/byclients")
+    public List<AccountDTO> getByClient(){
+        return accountRepository.findByClient_email("melba@mindhub.com").stream().map(AccountDTO::new).collect(Collectors.toList());
+    }*/
 }
--- src/main/java/com/mindhub/homebanking/repositories/AccountRepository.java
+++ src/main/java/com/mindhub/homebanking/repositories/AccountRepository.java
@@ -4,6 +4,11 @@ import com.mindhub.homebanking.models.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource
 public interface AccountRepository extends JpaRepository <Account,Long> {
+
+    List<Account> findByClient_id(long client_id);
+    List<Account> findByClient_email(String email);
 }
